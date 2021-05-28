1c2928c7d0acf641f9d560c115b3b99876d96bfb
David Morton <david.morton@smartpension.co.uk>
2021-05-28 14:35:06 +0100

    add spec for create_movie_worker

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Redis.exists_returns_integer = false
 
 module Smartflix
   class Application < Rails::Application
---
+++ spec/workers/create_movie_worker_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+
+RSpec.describe CreateMovieWorker do
+  subject { described_class.perform_async('worker_name', 5) }
+
+  Sidekiq::Testing.inline!
+
+  it 'adds a movie' do
+    expect{ subject }.to change { Movie.count }
+
+    expect(Movie.last.title).to be_present
+  end
+
+end

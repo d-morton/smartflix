89f33f59ff19ac06ab48aafbfd8879db9af4ce36
David Morton <david.morton@smartpension.co.uk>
2021-05-28 14:01:29 +0100

    configure sidekiq

--- app/workers/create_movie_worker.rb
+++ app/workers/create_movie_worker.rb
@@ -1,5 +1,6 @@
 class CreateMovieWorker
   include Sidekiq::Worker
+  sidekiq_options(retry: false, queue: 'movies')
 
   def perform(*args)
     movie = Movie.new(title: Faker::Movie.title)
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Redis.exists_returns_integer = false
 
 module Smartflix
   class Application < Rails::Application

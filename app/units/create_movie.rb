701baaeab207a91d86704929a66960ade8bbe8bc
David Morton <david.morton@smartpension.co.uk>
2021-06-04 13:18:39 +0100

    create movie

---
+++ app/units/create_movie.rb
@@ -0,0 +1,16 @@
+class CreateMovie
+
+  def initialize(data: {})
+    @data = data
+  end
+
+  def call
+    movie = Movie.new(title: data[:title])
+    movie.save
+  end
+
+  private
+
+  attr_reader :data
+
+end
